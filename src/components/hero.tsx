2bf7a79113fd7f73772ba825f6fc766b2d26bfe4
FAIZANAAZ779 <faixanaaz@gmail.com>
2025-01-26 23:38:44 +0500

    dashboard

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -61,7 +61,7 @@ export default  function Hero() {
                 
                 
                 <Image
-                    src={fetchdata||"/hero-background.jpeg"}
+                    src={fetchdata||"/hero-backg\round.jpeg"}
                     alt="hero image"
                     fill
                     className="object-contain md:object-cover"
