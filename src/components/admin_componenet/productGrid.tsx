568f8ea6f7f4f065665aec523954e2d53df20cfa
FAIZANAAZ779 <faixanaaz@gmail.com>
2025-02-02 22:16:03 +0500

    dashboard

--- src/components/admin_componenet/productGrid.tsx
+++ src/components/admin_componenet/productGrid.tsx
@@ -63,7 +63,7 @@ export default function ProductsGrid() {
   //----------------------------------------------- UseEffect
   useEffect(()=>{
     async function getData() {
-      let query = '*[_type == "products"]';
+      let query = `*[_type == "products"]`
       
       if (search) {
         query = `*[_type == "products" && name match "${search}*"]`;
@@ -122,7 +122,7 @@ export default function ProductsGrid() {
       <div className="flex items-center gap-4">
         <div className="relative flex-1">
           <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-          <Input type="search" placeholder="Search products..." className="pl-8 max-w-sm" value={search} onChange={(e: any)=>{setSearch(e.target.value)}}/>
+          <Input type="search" placeholder="Search products..." className="pl-8 max-w-sm" value={search} onChange={(e)=>{setSearch(e.target.value)}}/>
         </div>
         
         <Select defaultValue="all"
